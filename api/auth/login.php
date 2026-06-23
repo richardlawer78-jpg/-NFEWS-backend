2c59ed5d6cccafc5201759fff6790ab2dbe298fa
richardlawer <110224558+Rihgetrich7@users.noreply.github.com>
2026-06-23 05:05:17 +0000

    Fix BOM and db path issues

--- api/auth/login.php
+++ api/auth/login.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit;
 }
 
-require_once '../../config/db.php';
+require_once __DIR__ . '/../../config/db.php';
 
 $raw = file_get_contents("php://input");
 $data = json_decode($raw, true);
