14108aab7292b95669f735a07eb3c36bb37272dd
richardlawer <110224558+Rihgetrich7@users.noreply.github.com>
2026-06-23 05:16:17 +0000

    Remove BOM from PHP files

--- config/db.php
+++ config/db.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
