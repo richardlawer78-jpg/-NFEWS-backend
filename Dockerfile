484af3045337e7036fd0c91b50ff112837831bf6
richardlawer <110224558+Rihgetrich7@users.noreply.github.com>
2026-06-23 04:29:54 +0000

    Add router with CORS headers

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@
 RUN docker-php-ext-install pdo pdo_mysql mysqli
 COPY . /app
 WORKDIR /app
-CMD ["php", "-S", "0.0.0.0:8080"]
+CMD ["php", "-S", "0.0.0.0:8080", "router.php"]
---
+++ router.php
@@ -0,0 +1,18 @@
+﻿<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$file = __DIR__ . $uri;
+
+if (is_file($file)) {
+    return false;
+}
+
+include __DIR__ . $uri . '.php';
